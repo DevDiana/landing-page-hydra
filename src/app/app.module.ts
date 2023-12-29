ef23a9416ef24bcfb819ba6964d2067c3c3dbd34
Diana Regina <devdianaregina@gmail.com>
2023-12-28 21:04:45 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FooterModule } from './footer/footer.module';
+import { HeaderModule } from './header/header.module';
+import { BodyModule } from './body/body.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FooterModule,
+    HeaderModule,
+    BodyModule
   ],
   providers: [],
   bootstrap: [AppComponent]
